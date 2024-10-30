060361fc67cf91e0f2b7276e112315e4dbf1fe52
Evan Ferrao <evanferrao@gmail.com>
2024-10-30 08:05:59 +0530

    Add File Chooser Dialog for Selecting PDF Files

--- PDFMergerGUI.java
+++ PDFMergerGUI.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 
 public class PDFMergerGUI extends JFrame {
