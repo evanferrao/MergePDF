71646de7270a7569417c3166145ede2a2cf90936
Evan Ferrao <evanferrao@gmail.com>
2024-10-30 08:03:17 +0530

    Add JList to Display Selected PDF Files

--- PDFMergerGUI.java
+++ PDFMergerGUI.java
@@ -3,11 +3,18 @@ import java.awt.*;
 
 public class PDFMergerGUI extends JFrame {
 
+    private DefaultListModel<String> pdfListModel;
+
     public PDFMergerGUI() {
         setTitle("PDF Merger");
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+
+        pdfListModel = new DefaultListModel<>();
+        JList<String> pdfList = new JList<>(pdfListModel);
+        JScrollPane scrollPane = new JScrollPane(pdfList);
+        add(scrollPane, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
